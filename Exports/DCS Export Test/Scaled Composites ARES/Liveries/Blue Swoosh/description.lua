c75070095a686ae282f24c1e391752def704159d
ALASTAIR\Andrew <andrew.david.barth@gmail.com>
2022-03-13 20:41:57 -0700

    Added all files to the repo - this is the first git version of the project.

---
+++ Exports/DCS Export Test/Scaled Composites ARES/Liveries/Blue Swoosh/description.lua
@@ -0,0 +1,19 @@
+livery = {
+	{"Retopo AC .001", 0 ,"f-5e_nose_usaf_desert",false};
+	{"Retopo AC .001", DECAL ,"empty",true};
+
+	
+	{"F-5E_nose", 0 ,"f-5e_nose_usaf_desert",false};
+	{"F-5E_cockpit", 0 ,"F-5e_cockpit",true};
+	{"F-5E_tail", 0 ,"f-5e_tail_usaf_desert",false};
+	{"F-5E_rude", 0 ,"f-5e_rude_usaf_desert",false};
+	{"F-5E_wing", 0 ,"f-5e_wing_usaf_desert",false};
+	
+	{"pilot_F5_E", 0 ,"pilot_F-5e",true};
+	{"pilot_F5E_helmet", 0 ,"pilot_F-5e_helmet",true};
+	{"pilot_F15_patch", 0 ,"empty",true};
+}
+name = "Blue Swoosh Livery"
+countries = {
+	"USA"
+}
